5081a74189d3e0bb85a2500e70593af63ee4c23c
Shanming Liu <shanming.liu@ericsson.com>
2017-03-16 17:04:45 +0800

    Refactor

--- com/parse_html.py
+++ com/parse_html.py
@@ -5,6 +5,8 @@ from collections import namedtuple
 from bs4 import BeautifulSoup
 import pprint
 
+from utilty import cd
+
 ctrl_tuple = namedtuple('CtrlTuple', field_names=('seq', 'decimal', 'hexadecimal', 'abbreviation', 'name'))
 
 
@@ -12,9 +14,10 @@ def parse_html(source=None):
     res = list()
 
     if source is None:
-        source = os.path.join(os.path.dirname(__file__), 'resource/source_file.html')
-
-    soup = BeautifulSoup(open(source).read(), "html.parser")
+        with cd(os.path.dirname(__file__)):
+            soup = BeautifulSoup(open('resource/source_file.html').read(), "html.parser")
+    else:
+        soup = BeautifulSoup(open(source).read(), "html.parser")
     for tr in soup.body.table.find_all('tr')[1:]:
         data = [td.text for td in tr.find_all('td')]
         data[1] = int(data[1])
